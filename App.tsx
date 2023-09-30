96d57cd361a1cf1fb347c8fe262061cbada25740
P4CM4N890 <p4cm4n890@gmail.com>
2023-09-30 17:57:59 -0600

    Actualización del proyecto

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+function App(): JSX.Element {
+
+  return (
+    <SafeAreaView>
+      <Text>Safi 2.0</Text>
+      <Text>Safi 2.0</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
