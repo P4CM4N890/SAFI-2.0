d905c658d0a49671bf70c6d371e7043096f98d4c
Fernando LR <fernando.lorenzo1899@gmail.com>
2023-10-19 12:00:00 -0600

    comenzar la navegación

--- App.tsx
+++ App.tsx
@@ -1,23 +1,27 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
 import { SafeAreaView } from 'react-native';
+import { NavigationContainer, DefaultTheme  } from '@react-navigation/native';
 
-import { LoginScreen } from './src/screens/LoginScreen';
-// import { SignUpScreen } from './src/screens/SignUpScreen';
-// import { ForgotPasswordScreen } from './src/screens/ForgotPasswordScreen';
-// import { TokenVerificationScreen } from './src/screens/TokenVerificationScreen';
-// import { ResetPasswordScreen } from './src/screens/ResetPasswordScreen';
+import { AuthStackNavigator } from './src/navigation/AuthStackNavigator';
+
+const theme = {
+    ...DefaultTheme,
+    colors : {
+        ...DefaultTheme.colors,
+        background: '#111'
+    }
+}
 
 function App(): JSX.Element {
-
     return (
         <SafeAreaView>
-            <LoginScreen/>
-            {/* <SignUpScreen/> */}
-            {/* <ForgotPasswordScreen/> */}
-            {/* <TokenVerificationScreen/> */}
-            {/* <ResetPasswordScreen /> */}
+            <NavigationContainer theme={ theme }>
+                <AuthStackNavigator />
+            </NavigationContainer>
         </SafeAreaView>
     );
-}
+};
 
 export default App;
---
+++ src/navigation/AuthStackNavigator.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { LoginScreen } from '../screens/LoginScreen';
+import { SignUpScreen } from '../screens/SignUpScreen';
+import { ResetPasswordScreen } from '../screens/ResetPasswordScreen';
+import { TokenVerificationScreen } from '../screens/TokenVerificationScreen';
+
+const AuthStack = createStackNavigator();
+
+export const AuthStackNavigator = () => {
+    return (
+        <AuthStack.Navigator
+            screenOptions={{
+                headerShown: false
+            }}
+        >
+
+            <AuthStack.Screen name='LoginScreen' options={{ title: 'Login' }} component={ LoginScreen }/>
+            <AuthStack.Screen name='SignUpScreen' options={{ title: 'SignUp' }} component={ SignUpScreen }/>
+            <AuthStack.Screen name='ResetPasswordScreen' options={{ title: 'Reset Password' }} component={ ResetPasswordScreen }/>
+            <AuthStack.Screen name='TokenVerificationScreen' options={{ title: 'Token Verification' }} component={ TokenVerificationScreen }/>
+        
+        </AuthStack.Navigator>
+    );
+};
