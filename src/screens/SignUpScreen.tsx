da57ab54669083a360f43fc81f4f4d8a592263ee
Fernando LR <fernando.lorenzo1899@gmail.com>
2023-10-22 17:00:40 -0600

    agregar icono a la pantalla de signup

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { View, KeyboardAvoidingView, Text } from 'react-native';
+import { View, KeyboardAvoidingView, Text, ScrollView } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 
+import { Logo } from '../assets/Logo';
 import { WaveTop } from '../assets/WaveTop';
 import { WaveBottom } from '../assets/WaveBottom';
 
-import { InputLabel } from '../components/inputs/InputLabel';
 import { Button } from '../components/buttons/Button';
+import { InputLabel } from '../components/inputs/InputLabel';
+import { BackButton } from '../components/buttons/BackButton';
 import { TransparentButton } from '../components/buttons/TransparentButton';
 import { DatePickerLabel } from '../components/datepickers/DatePickerLabel';
 
@@ -16,58 +18,68 @@ export const SignUpScreen = ({ navigation }: Props) => {
 
     return (
         <KeyboardAvoidingView className='w-full h-full'>
-            <WaveTop/>
-            <View className='w-full h-full items-center justify-center z-10 py-6'>
-                <Text className='text-3xl font-bold text-primary uppercase tracking-tight'>Crear Cuenta</Text>
+            <ScrollView>
+                <WaveTop/>
 
-                <InputLabel 
-                    label='Nombre de usuario' 
-                    placeholder='' 
-                    type='text'
-                    extraClass='mt-12'
-                />
-                <InputLabel 
-                    label='Correo electrónico' 
-                    placeholder='' 
-                    type='email'
-                    extraClass='mt-6'
-                />
+                <View className='w-full h-full flex items-center justify-center z-10 py-6 mt-5'>
+                    <BackButton 
+                        iconColor='#000' 
+                        iconSize={ 30 } 
+                        extraClass='bg-white'
+                        onPress={ () => navigation.goBack() }
+                    />
 
-                <DatePickerLabel 
-                    label='Fecha de nacimiento' 
-                    mode='date'
-                    extraClass='mt-6'
-                />
+                    <Logo width={ 138 } height={ 128 } extraClass='-mt-5'/>
 
-                <InputLabel 
-                    label='Contraseña' 
-                    placeholder='' 
-                    type='text'
-                    secureTextEntry
-                    extraClass='mt-6'
-                />
-                <InputLabel 
-                    label='Confirmar contraseña' 
-                    placeholder='' 
-                    type='text'
-                    secureTextEntry
-                    extraClass='mt-6'
-                />
+                    <Text className='text-3xl font-bold text-primary uppercase tracking-tight mt-4'>Crear Cuenta</Text>
 
-                <Button 
-                    label='Registrarme' 
-                    extraClass='mt-8'
-                    onPress={ () => {} }
-                />
+                    <InputLabel 
+                        label='Nombre de usuario' 
+                        placeholder='' 
+                        type='text'
+                        extraClass='mt-12'
+                    />
+                    <InputLabel 
+                        label='Correo electrónico' 
+                        placeholder='' 
+                        type='email'
+                        extraClass='mt-6'
+                    />
+                    <DatePickerLabel 
+                        label='Fecha de nacimiento' 
+                        mode='date'
+                        extraClass='mt-6'
+                    />
+                    <InputLabel 
+                        label='Contraseña' 
+                        placeholder='' 
+                        type='text'
+                        secureTextEntry
+                        extraClass='mt-6'
+                    />
+                    <InputLabel 
+                        label='Confirmar contraseña' 
+                        placeholder='' 
+                        type='text'
+                        secureTextEntry
+                        extraClass='mt-6'
+                    />
 
-                <TransparentButton 
-                    label='¿Ya tienes una cuenta? Inicia Sesión' 
-                    textStyle='text-black text-sm mt-3' 
-                    extraClass='mt-4'
-                    onPress={ () => navigation.navigate('LoginScreen') }
-                />
-            </View>
-            <WaveBottom/>
+                    <Button 
+                        label='Registrarme' 
+                        extraClass='mt-6'
+                        onPress={ () => {} }
+                    />
+
+                    <TransparentButton 
+                        label='¿Ya tienes una cuenta? Inicia Sesión' 
+                        textStyle='text-black text-sm mt-3' 
+                        extraClass='mt-4'
+                        onPress={ () => navigation.navigate('LoginScreen') }
+                    />
+                </View>
+                <WaveBottom/>
+            </ScrollView>
         </KeyboardAvoidingView>
     );
 }
