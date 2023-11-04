ac805f8d0f5f3f95b78e2f6146dff8c319359499
Fernando LR <fernando.lorenzo1899@gmail.com>
2023-11-04 16:22:09 -0600

    agregar iconos al tab navigator

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import { HomeScreen } from '../screens/HomeScreen';
 import { IncomeScreen } from '../screens/IncomeScreen';
+import { TabBarButton } from '../components/buttons/TabBarButton';
 
 const Tab = createBottomTabNavigator();
 
@@ -10,11 +11,48 @@ export const BottomTabNavigator = () => {
         <Tab.Navigator
             initialRouteName='HomeScreen'
             screenOptions={{
-                headerShown: false
+                headerShown: false,
+                tabBarActiveTintColor: '#4F33D8',
+                tabBarInactiveTintColor: '#000',   
+                tabBarShowLabel: false,
+
+                tabBarStyle: {
+                    height: 65,
+                }
             }}
         >
-            <Tab.Screen name='HomeScreen' options={{ title: 'Inicio' }}  component={ HomeScreen } />
-            <Tab.Screen name='IncomeScreen' options={{ title: 'Ingresos' }} component={ IncomeScreen } />
+            <Tab.Screen 
+                name='HomeScreen' 
+                component={ HomeScreen } 
+                options={{ 
+                    title: 'Inicio',
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <TabBarButton 
+                            iconColor={ color } 
+                            iconSize={ size } 
+                            isFocused={ focused }
+                            iconName='home-outline'
+                            label='Inicio'
+                        />
+                    )
+                }}    
+            />
+            <Tab.Screen 
+                name='IncomeScreen' 
+                component={ IncomeScreen }
+                options={{
+                    title: 'Ingresos',
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <TabBarButton 
+                            iconColor={ color } 
+                            iconSize={ size } 
+                            isFocused={ focused }
+                            iconName='cash-outline'
+                            label='Ingresos'
+                        />
+                    )
+                }} 
+            />
         </Tab.Navigator>
     );
 }
