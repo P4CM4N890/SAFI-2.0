72daaa3d2f76ed003251636ac9d69eb883cf118a
Fernando LR <fernando.lorenzo1899@gmail.com>
2023-11-04 19:18:25 -0600

    boton de agregar ingreso o meta

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import { AddButton } from '../components/buttons/AddButton';
+
 export const HomeScreen = () => {
     return (
         <View className='w-full h-full flex justify-center items-center'>
             <Text>HomeScreen</Text>
+            <AddButton />
         </View>
     );
 }
