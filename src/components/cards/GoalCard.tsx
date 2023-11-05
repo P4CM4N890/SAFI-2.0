aac7e8e0f334aab67aea42171558233553ab7c26
Fernando LR <fernando.lorenzo1899@gmail.com>
2023-11-05 14:41:23 -0600

    primera version de la screen de metas

--- src/components/cards/GoalCard.tsx
+++ src/components/cards/GoalCard.tsx
@@ -10,9 +10,10 @@ interface Props {
     iconColor: string;
     totalGoalAmount: string;
     totalGoalCompleted: string;
+    progress: number;
 }
 
-export const GoalCard = ({ title, iconName, iconColor, totalGoalAmount, totalGoalCompleted }: Props) => {
+export const GoalCard = ({ title, iconName, iconColor, totalGoalAmount, totalGoalCompleted, progress }: Props) => {
     return (
         <View className='w-full flex-row items-center bg-white rounded-2xl p-2 border-2 border-slate-200 mt-2'>
             <View className='w-3/4 flex-row items-center gap-x-3'>
@@ -27,7 +28,7 @@ export const GoalCard = ({ title, iconName, iconColor, totalGoalAmount, totalGoa
 
                 <View className='w-full justify-center'>
                     <Text className='text-black font-bold text-lg'>{ title }</Text>
-                    <Bar progress={ 0.5 } height={ 10 } color={ iconColor }/>
+                    <Bar progress={ progress } height={ 10 } color={ iconColor } unfilledColor='#D9D9D9' borderColor='#D9D9D9'/>
                 </View>
             </View>
 
--- src/screens/GoalsScreen.tsx
+++ src/screens/GoalsScreen.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, ScrollView } from 'react-native';
 
 import { Header } from '../components/headers/Header';
 import { GoalCard } from '../components/cards/GoalCard';
+import { MainGoalCard } from '../components/cards/MainGoalCard';
 
 export const GoalsScreen = () => {
     return (
@@ -13,6 +14,13 @@ export const GoalsScreen = () => {
             >
                 <Header title='Tus Metas' extraClass='text-2xl'/>
 
+                <MainGoalCard 
+                    title='Laptop Asus' 
+                    startDate='12/Octubre/2023'
+                    endDate='12/Diciembre/2023'
+                    progress={ 0.5 }
+                />
+
                 <View className='mt-6'>
                     <GoalCard 
                         title='Laptop' 
@@ -20,6 +28,7 @@ export const GoalsScreen = () => {
                         iconColor='#D8336A'
                         totalGoalCompleted='1000.00'
                         totalGoalAmount='2000.00'
+                        progress={ 0.5 }
                     />
                     <GoalCard 
                         title='Celular' 
@@ -27,6 +36,7 @@ export const GoalsScreen = () => {
                         iconColor='#33D8A2'
                         totalGoalCompleted='1000.00'
                         totalGoalAmount='2000.00'
+                        progress={ 0.5 }
                     />
                     <GoalCard 
                         title='Monitor' 
@@ -34,6 +44,7 @@ export const GoalsScreen = () => {
                         iconColor='#A233D8'
                         totalGoalCompleted='1000.00'
                         totalGoalAmount='2000.00'
+                        progress={ 0.5 }
                     />
                     <GoalCard 
                         title='Zapatos' 
@@ -41,6 +52,7 @@ export const GoalsScreen = () => {
                         iconColor='#75E2F8'
                         totalGoalCompleted='1000.00'
                         totalGoalAmount='2000.00'
+                        progress={ 0.5 }
                     />
                 </View>
 
