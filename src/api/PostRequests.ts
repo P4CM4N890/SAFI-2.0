98e35ad430b8a0f9115754ea59911aafc5f0127e
Raul Ochoa <raulochoa039@gmail.com>
2023-11-20 19:34:11 -0600

    Agregar peticiones POST

--- src/api/PostRequests.ts
+++ src/api/PostRequests.ts
@@ -1,6 +1,8 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
-import { apiInstance } from './instance';
-import { InicioSesion, MetaCreate, UsuarioCreate } from '../interfaces/ApiInterfaces'
+import { apiInstance, checkToken } from './instance';
+import { AbonoCreate, CategoriaCreate, GastoCreate, IngresoCreate, 
+    InicioSesion, MetaCreate, UsuarioCreate, ValidarToken, RecordatorioDePagoCreate, 
+    PreguntaCreate, RespuestaCreate, MetaFijadaCreate, PredictorObject } from '../interfaces/ApiInterfaces';
 
 export const login = async (correo: string, password: string): 
     Promise<AxiosResponse> => {
@@ -20,6 +22,7 @@ export const login = async (correo: string, password: string):
 
     try {
         const response = await apiInstance.post(url, body, config);
+
         return response;
     }
     catch (err) {
@@ -33,6 +36,64 @@ export const login = async (correo: string, password: string):
     }
 };
 
+export const generarTokenRecuperacion = async (id_usuario: number): Promise<AxiosResponse> => {
+    let config = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    let url = "/token/generar_token/";
+
+    let body = {
+        id_usuario
+    };
+
+    try {
+        const response = await apiInstance.post(url, body, config);
+
+        return response;
+    }
+    catch (err) {
+        const errors = err as Error | AxiosError;
+        
+        if(!axios.isAxiosError(errors)){
+            throw new Error(errors.message);
+        }
+
+        throw new Error(errors.response?.data?.detail);
+    }
+};
+
+export const validarTokenRecuperacion = async (token: ValidarToken): Promise<AxiosResponse> => {
+    let config = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    let url = "/token/validar_token/";
+
+    let body = {
+        ...token
+    };
+
+    try {
+        const response = await apiInstance.post(url, body, config);
+
+        return response;
+    }
+    catch (err) {
+        const errors = err as Error | AxiosError;
+        
+        if(!axios.isAxiosError(errors)){
+            throw new Error(errors.message);
+        }
+
+        throw new Error(errors.response?.data?.detail);
+    }
+}
+
 export const crearUsuario = async (usuario: UsuarioCreate): Promise<AxiosResponse> => {
     let config = {
         headers: {
@@ -40,14 +101,15 @@ export const crearUsuario = async (usuario: UsuarioCreate): Promise<AxiosRespons
         },
     };
 
-    let url = "/usuario/"
+    let url = "/usuario/";
 
     let body = {
         ...usuario,
-    }
+    };
 
     try {
         const response = await apiInstance.post(url, body, config);
+
         return response;
     }
     catch (err) {
@@ -62,20 +124,312 @@ export const crearUsuario = async (usuario: UsuarioCreate): Promise<AxiosRespons
 }
 
 export const crearMeta = async (meta: MetaCreate): Promise<AxiosResponse> => {
+    const token = await checkToken();
+
     let config = {
         headers: {
             'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
         },
     };
 
-    let url = "/meta/"
+    let url = "/meta/";
 
     let body = {
         ...meta,
-    }
+    };
 
     try {
         const response = await apiInstance.post(url, body, config);
+
+        return response;
+    }
+    catch (err) {
+        const errors = err as Error | AxiosError;
+        
+        if(!axios.isAxiosError(errors)){
+            throw new Error(errors.message);
+        }
+
+        throw new Error(errors.response?.data?.detail);
+    }
+}
+
+export const crearCategoria = async (categoria: CategoriaCreate): Promise<AxiosResponse> => {
+    const token = await checkToken();
+
+    let config = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+    };
+
+    let url = "/categoria/";
+
+    let body = {
+        ...categoria,
+    };
+
+    try {
+        const response = await apiInstance.post(url, body, config);
+
+        return response;
+    }
+    catch (err) {
+        const errors = err as Error | AxiosError;
+        
+        if(!axios.isAxiosError(errors)){
+            throw new Error(errors.message);
+        }
+
+        throw new Error(errors.response?.data?.detail);
+    }
+}
+
+export const crearIngreso = async (ingreso: IngresoCreate): Promise<AxiosResponse> => {
+    const token = await checkToken();
+
+    let config = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+    };
+
+    let url = "/ingreso/";
+
+    let body = {
+        ...ingreso,
+    };
+
+    try {
+        const response = await apiInstance.post(url, body, config);
+
+        return response;
+    }
+    catch (err) {
+        const errors = err as Error | AxiosError;
+        
+        if(!axios.isAxiosError(errors)){
+            throw new Error(errors.message);
+        }
+
+        throw new Error(errors.response?.data?.detail);
+    }
+}
+
+export const crearAbono = async (abono: AbonoCreate): Promise<AxiosResponse> => {
+    const token = await checkToken();
+
+    let config = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+    };
+
+    let url = "/abono/";
+
+    let body = {
+        ...abono,
+    };
+
+    try {
+        const response = await apiInstance.post(url, body, config);
+
+        return response;
+    }
+    catch (err) {
+        const errors = err as Error | AxiosError;
+        
+        if(!axios.isAxiosError(errors)){
+            throw new Error(errors.message);
+        }
+
+        throw new Error(errors.response?.data?.detail);
+    }
+}
+
+export const crearGasto = async (gasto: GastoCreate): Promise<AxiosResponse> => {
+    const token = await checkToken();
+
+    let config = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+    };
+
+    let url = "/gasto/";
+
+    let body = {
+        ...gasto,
+    };
+
+    try {
+        const response = await apiInstance.post(url, body, config);
+
+        return response;
+    }
+    catch (err) {
+        const errors = err as Error | AxiosError;
+        
+        if(!axios.isAxiosError(errors)){
+            throw new Error(errors.message);
+        }
+
+        throw new Error(errors.response?.data?.detail);
+    }
+}
+
+export const crearRecordatorio = async (recordatorio: RecordatorioDePagoCreate): Promise<AxiosResponse> => {
+    const token = await checkToken();
+
+    let config = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+    };
+
+    let url = "/recordatorio/";
+
+    let body = {
+        ...recordatorio,
+    };
+
+    try {
+        const response = await apiInstance.post(url, body, config);
+
+        return response;
+    }
+    catch (err) {
+        const errors = err as Error | AxiosError;
+        
+        if(!axios.isAxiosError(errors)){
+            throw new Error(errors.message);
+        }
+
+        throw new Error(errors.response?.data?.detail);
+    }
+}
+
+export const crearPregunta = async (pregunta: PreguntaCreate): Promise<AxiosResponse> => {
+    const token = await checkToken();
+
+    let config = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+    };
+
+    let url = "/pregunta/";
+
+    let body = {
+        ...pregunta,
+    };
+
+    try {
+        const response = await apiInstance.post(url, body, config);
+
+        return response;
+    }
+    catch (err) {
+        const errors = err as Error | AxiosError;
+        
+        if(!axios.isAxiosError(errors)){
+            throw new Error(errors.message);
+        }
+
+        throw new Error(errors.response?.data?.detail);
+    }
+}
+
+export const crearRespuesta = async (respuesta: RespuestaCreate): Promise<AxiosResponse> => {
+    const token = await checkToken();
+
+    let config = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+    };
+
+    let url = "/respuesta/";
+
+    let body = {
+        ...respuesta,
+    };
+
+    try {
+        const response = await apiInstance.post(url, body, config);
+
+        return response;
+    }
+    catch (err) {
+        const errors = err as Error | AxiosError;
+        
+        if(!axios.isAxiosError(errors)){
+            throw new Error(errors.message);
+        }
+
+        throw new Error(errors.response?.data?.detail);
+    }
+}
+
+export const crearMetaFijada = async (metaFijada: MetaFijadaCreate): Promise<AxiosResponse> => {
+    const token = await checkToken();
+
+    let config = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+    };
+
+    let url = "/metaFijada/";
+
+    let body = {
+        ...metaFijada,
+    };
+
+    try {
+        const response = await apiInstance.post(url, body, config);
+
+        return response;
+    }
+    catch (err) {
+        const errors = err as Error | AxiosError;
+        
+        if(!axios.isAxiosError(errors)){
+            throw new Error(errors.message);
+        }
+
+        throw new Error(errors.response?.data?.detail);
+    }
+}
+
+export const predecirMeta = async (predictor: PredictorObject): Promise<AxiosResponse> => {
+    const token = await checkToken();
+
+    let config = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+    };
+
+    let url = "/predictorMetas/";
+
+    let body = {
+        ...predictor,
+    };
+
+    try {
+        const response = await apiInstance.post(url, body, config);
+
         return response;
     }
     catch (err) {
