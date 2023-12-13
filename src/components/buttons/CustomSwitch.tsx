a3aa49c706e73388c147848a7a4df82e75d2da86
Fernando LR <fernando.lorenzo1899@gmail.com>
2023-12-12 20:50:54 -0600

    agregar pantalla de notificaciones

--- src/components/buttons/CustomSwitch.tsx
+++ src/components/buttons/CustomSwitch.tsx
@@ -3,20 +3,22 @@ import { Switch } from 'react-native';
 
 interface props {
     isOn: boolean;
+    scale?: number;
+    color?: string;
 }
 
-export const CustomSwitch = ({ isOn }: props) => {
+export const CustomSwitch = ({ isOn, color = '#4F33D8', scale = 1.3 }: props) => {
     const [ isEnabled, setIsEnabled ] = useState(isOn);
 
     const toggleSwitch = () => setIsEnabled(!isEnabled);
 
     return (
         <Switch 
-            trackColor={{ false: '#D9D9D9', true: '#4F33D8' }}
+            trackColor={{ false: '#D9D9D9', true: color }}
             thumbColor='#FCFCFC'
             onValueChange={ toggleSwitch }
             value={ isEnabled }
-            style={{ transform: [{ scaleX: 1.3 }, { scaleY: 1.3 }] }}
+            style={{ transform: [{ scaleX: scale }, { scaleY: scale }] }}
         />
     )
 }
--- src/screens/NotificationsScreen.tsx
+++ src/screens/NotificationsScreen.tsx
@@ -1,12 +1,48 @@
 import React from 'react';
 import { View, Text, ScrollView } from 'react-native';
 
-export const NotificationsScreen = () => {
+import { AddNotificationButton } from '../components/buttons/AddNotificationButton';
+import { BackButton } from '../components/buttons/BackButton';
+import { NotificationCard } from '../components/cards/NotificationCard';
+
+import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
+
+interface Props extends BottomTabScreenProps<any, any> {};
+
+export const NotificationsScreen = ({ navigation }: Props) => {
+
     return (
-        <ScrollView showsVerticalScrollIndicator={ false }>
-            <View className='w-full h-full items-center p-5'>
-                <Text className='text-center'>Notificaciones</Text>
+        <View className='w-full h-full items-center p-5'>
+
+            <BackButton 
+                iconColor='#FFF' 
+                iconSize={ 30 } 
+                extraClass='bg-primary'
+                onPress={ () => navigation.goBack() }
+            />
+            <Text className='text-black text-2xl font-semibold mt-2 tracking-widest'>Notificaciones</Text>
+
+            <View className='mt-6'>
+                <ScrollView>
+                    <NotificationCard 
+                        title='Abono para celular'
+                        iconName='calendar-outline'
+                        iconColor='#33D8A2'
+                        date='31/01/2024'
+                        time='12:00 p.m.'
+                    />
+
+                    <NotificationCard 
+                        title='Abono para laptop'
+                        iconName='calendar-outline'
+                        iconColor='#33D8A2'
+                        date='31/01/2024'
+                        time='12:00 p.m.'
+                    />
+                </ScrollView>
             </View>
-        </ScrollView>
+
+            <AddNotificationButton />
+        </View>
     );
 }
