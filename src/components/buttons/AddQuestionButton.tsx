e51edf90a5fa649d8ef7e70ab249701d9b7af644
Fernando LR <fernando.lorenzo1899@gmail.com>
2023-12-15 23:42:29 -0600

    modal para crear pregunta

--- src/components/buttons/AddQuestionButton.tsx
+++ src/components/buttons/AddQuestionButton.tsx
@@ -3,14 +3,18 @@ import { View, TouchableOpacity } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
-export const AddQuestionButton = () => {
+interface Props {
+    onPress: () => void;
+}
+
+export const AddQuestionButton = ({ onPress }: Props) => {
 
     return (
         <View className='items-center absolute bottom-5 right-5'>
             <TouchableOpacity 
                 className='bg-primary w-16 h-16 justify-center items-center rounded-full z-10' 
                 activeOpacity={ 0.8 }
-                onPress={ () => {}}
+                onPress={ onPress }
             >
                 <Icon name='add-outline' size={ 40 } color='#FFF'/>
             </TouchableOpacity>
--- src/screens/ForumScreen.tsx
+++ src/screens/ForumScreen.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, ScrollView } from 'react-native';
+
+import Modal from "react-native-modal";
 
 import { AddQuestionButton } from '../components/buttons/AddQuestionButton';
 import { RankingButton } from '../components/buttons/RankingButton';
 import { HomeButton } from '../components/buttons/HomeButton';
 import { ProfileButton } from '../components/buttons/ProfileButton';
 import { QuestionCard } from '../components/cards/QuestionCard';
+import { InputLabel } from '../components/inputs/InputLabel';
+import { Button } from '../components/buttons/Button';
 
 export const ForumScreen = () => {
+
+    const [ modalVisible, setModalVisible ] = useState(false);
+
+    const openModal = () => {
+      setModalVisible(true);
+    };
+  
+    const closeModal = () => {
+      setModalVisible(false);
+    };
+
     return (
         <View className='w-full h-full items-center p-5'>
             <ScrollView 
@@ -36,12 +51,47 @@ export const ForumScreen = () => {
                     />
 
                 </View>
+
+                <Modal 
+                    isVisible={ modalVisible }
+                    animationIn={ 'bounceIn' }
+                >
+                    <View className='bg-white w-full rounded-2xl items-center py-5'>
+                        <Text className='text-primary font-bold text-2xl uppercase tracking-wider'>Crear Pregunta</Text>
+                    
+                        <InputLabel 
+                            type='text'
+                            label='Título'
+                            extraClass='mt-7'
+                        />
+
+                        <InputLabel 
+                            type='text'
+                            label='Descripción'
+                            extraClass='mt-5'
+                        />
+
+                        <View className='w-5/6 mt-16 flex-row justify-between'>
+                            <Button 
+                                label='Publicar' 
+                                onPress={ () => {} }
+                            />
+                            <Button 
+                                label='Cancelar' 
+                                extraClass='bg-rose-600'
+                                onPress={ closeModal }
+                            />
+                        </View>
+
+                    </View>
+                </Modal>
+
             </ScrollView>
 
             <ProfileButton />
             <HomeButton />
             <RankingButton />
-            <AddQuestionButton />
+            <AddQuestionButton onPress={ openModal }/>
         </View>
     );
 }
