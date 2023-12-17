6f00896533ffd365bd21d2152bddf8470bfcb42e
Fernando LR <fernando.lorenzo1899@gmail.com>
2023-12-16 23:40:53 -0600

    conectar el boton de home

--- src/components/buttons/HomeButton.tsx
+++ src/components/buttons/HomeButton.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { View, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
 export const HomeButton = () => {
+
+    const navigation = useNavigation<any>();
 
     return (
         <View className='items-center absolute top-5 right-4'>
             <TouchableOpacity 
                 className='bg-white z-10' 
                 activeOpacity={ 0.8 }
-                onPress={ () => {} }
+                onPress={ () => navigation.navigate('HomeScreen') }
             >
                 <Icon name='home' size={ 50 } color='#4F33D8'/>
             </TouchableOpacity>
