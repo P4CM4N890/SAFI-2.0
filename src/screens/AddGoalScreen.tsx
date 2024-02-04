de88061334362683dc16734dc3dbe1c84fca0548
Fernando LR <fernandolr.0915@gmail.com>
2024-02-04 15:43:26 -0600

    agregar mensajes de error

--- src/screens/AddGoalScreen.tsx
+++ src/screens/AddGoalScreen.tsx
@@ -9,7 +9,7 @@ import { useForm, useUiStore } from '../hooks';
 import { addGoal } from '../store/goals/thunks';
 
 import { InputLabel, Button, DatePickerLabel, CustomSwitch, CategoryModal,
-ColorModal, PriorityModal } from '../components';
+ColorModal, PriorityModal, ErrorMessage } from '../components';
 
 import { categoryIcon, iconColor, priority, priorityColor } from '../types/appTypes';
 
@@ -54,14 +54,24 @@ export const AddGoalScreen = ({ navigation }: Props) => {
     }, []);
 
     const onAddGoal = () => {
-        if (!nombre || !cantidad || !fecha_fin || !fecha_inicio) { 
-            setError('Debes completar todos los campos.');
-            console.log('Debes completar todos los campos.');
+        if (!nombre) { 
+            setError('El nombre es obligatorio.');
             return;
 
-        } else if (Number(cantidad) <= 0) {
-            setError('La cantidad debe ser mayor a cero.');
-            console.log('La cantidad debe ser mayor a cero.');
+        } else if (!fecha_inicio) {
+            setError('La fecha de inicio es obligatoria.');
+            return;
+
+        } else if (!fecha_fin) {
+            setError('La fecha de finalización es obligatoria.');
+            return;
+
+        } else if (!cantidad) {
+            setError('La meta a alcanzar es obligatoria.');
+            return;
+
+        }else if (Number(cantidad) <= 0) {
+            setError('La meta a alcanzar debe ser mayor a cero.');
             return;
         }
 
@@ -81,6 +91,10 @@ export const AddGoalScreen = ({ navigation }: Props) => {
                 completada: 0            
             }, fijar === 'si' ? true : false)
         );
+    };
+
+    const isErrorOfField = (field: string) => {
+        return error.includes(field);
     };
 
     const openCategoryModal = () => {
@@ -156,17 +170,29 @@ export const AddGoalScreen = ({ navigation }: Props) => {
                         value={ nombre }
                         onChange={ (value) => onChange(value, 'nombre') }
                     />
+                    <ErrorMessage
+                        message={ error }
+                        showMessage={ !!error && isErrorOfField('nombre')}
+                    />
 
                     <DatePickerLabel 
                         label='Fecha de inicio'
                         extraClass='mt-3'
                         onChange={ (value) => onChange(value, 'fecha_inicio') }
                     />
+                    <ErrorMessage
+                        message={ error }
+                        showMessage={ !!error && isErrorOfField('fecha de inicio')}
+                    />
 
                     <DatePickerLabel 
                         label='Fecha de finalización'
                         extraClass='mt-3'
                         onChange={ (value) => onChange(value, 'fecha_fin') }
+                    />
+                    <ErrorMessage
+                        message={ error }
+                        showMessage={ !!error && isErrorOfField('fecha de finalización')}
                     />
 
                     <InputLabel 
@@ -177,6 +203,10 @@ export const AddGoalScreen = ({ navigation }: Props) => {
                         iconName='cash-outline'
                         value={ cantidad }
                         onChange={ (value) => onChange(value, 'cantidad') }
+                    />
+                    <ErrorMessage
+                        message={ error }
+                        showMessage={ !!error && isErrorOfField('meta a alcanzar')}
                     />
 
                     <InputLabel 
